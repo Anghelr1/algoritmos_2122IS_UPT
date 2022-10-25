433c092e31a848c4603a0aaad459951b2894aec7
Anghelr1 <anghelgr14@gmail.com>
2022-10-24 23:50:04 -0500

    Medio hecho con arraylist

--- Parcial 1/Unidad 2/Prácticas/P6. Battalla Naval/src/Main.java
+++ Parcial 1/Unidad 2/Prácticas/P6. Battalla Naval/src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
@@ -12,8 +14,8 @@ public class Main {
         System.out.print("Ingresa las columnas del tablero: ");
         int tableroColumnas = scanner.nextInt();
         System.out.print("Ingresa las filas del tablero: ");
-        int tablefroFilas = scanner.nextInt();
-        if (tableroColumnas < 4 || tablefroFilas < 3 ){
+        int tableroFilas = scanner.nextInt();
+        if (tableroColumnas < 4 || tableroFilas < 3) {
             System.out.println("El tablero debe ser de un tamaño mínimo de 4 x 3");
             System.exit(0);
         }
@@ -21,13 +23,13 @@ public class Main {
         //Datos para jugabilidad
         System.out.print("Ingresa tu modo de juego:\n 1. Un solo jugador.\n 2. Dos jugadores.\n Opcion: ");
         int modoJuego = scanner.nextInt();
-        if (modoJuego != 1 && modoJuego != 2){
+        if (modoJuego != 1 && modoJuego != 2) {
             System.out.println("Opción incorrecta");
             System.exit(0);
         }
         System.out.println("Cuantos barcos quieres:");
         int nbarcos = scanner.nextInt();
-        if (nbarcos < 2){
+        if (nbarcos < 2) {
             System.out.println("El numero mínimo de barcos es de 2");
             System.exit(0);
         }
@@ -51,8 +53,8 @@ public class Main {
 
         }
         //Imprimiendo columnas y relleno de filas
-        for (int i = 1; i <= tablefroFilas; i++) {
-            for (int j = 1; j <= tablefroFilas; j++) {
+        for (int i = 1; i <= tableroFilas; i++) {
+            for (int j = 1; j <= tableroFilas; j++) {
                 if (j <= 1) {
                     // Si i es de solo un dígito se le concatena un 0
                     if (i < 10) {
@@ -62,20 +64,19 @@ public class Main {
                 }
 
                 // Imprimiendo casillas de tablero dependiendo la diferencia entre columnas y filas
-                if (tableroColumnas == tablefroFilas) {
+                if (tableroColumnas == tableroFilas) {
                     System.out.print("*" + "  ");
-                }
-                if (tableroColumnas > tablefroFilas) {
+                } else if (tableroColumnas > tableroFilas) {
                     System.out.print("*" + "  ");
-                    if (j == tablefroFilas) {
-                        int dif = tableroColumnas - tablefroFilas;
+                    if (j == tableroFilas) {
+                        int dif = tableroColumnas - tableroFilas;
                         for (int k = 1; k <= dif; k++) {
                             System.out.print("*" + "  ");
                         }
                     }
                 } else {
-                    if (j == tablefroFilas) {
-                        int dif = tablefroFilas - tableroColumnas;
+                    if (j == tableroFilas) {
+                        int dif = tableroFilas - tableroColumnas;
                         for (int k = 1; k <= dif; k++) {
                             System.out.print("*" + "  ");
                         }
@@ -87,145 +88,103 @@ public class Main {
         }
 
         //================ JUGABILIDAD ================
-        int nCasillas = tableroColumnas * tablefroFilas;
+        int nCasillas = tableroColumnas * tableroFilas;
 
-        if (modoJuego == 1){
-            // Coordenadas random
-            int randomY = (int)(Math.random()*nCasillas+1);
-            int randomX = (int)(Math.random()*nCasillas+1);
+        if (modoJuego == 1) {
 
-            //Aisgnando barcos jugador 1
-            int[] posicionYp1 = new int[nbarcos];
-            int[] posicionXp1 = new int[nbarcos];
-
-            //Almacenando posiciones del jugador 1
-            System.out.println("Turno de que el jugador 1 asigne sus barcos");
-            for (int i = 1; i <= nbarcos; i++){
-                System.out.println("Asigna posición barco " + i);
-                System.out.print("Posición Y: ");
-                posicionYp1[i - 1] = scanner.nextInt();
-                // Verificamos que sus coordenadas existan
-                if (posicionYp1[i - 1] > tablefroFilas){
-                    System.out.println("Tu coordenada Y no existe, intenta de nuevo:");
-                    posicionYp1[i - 1] = scanner.nextInt();
-                }
-                System.out.print("Posición X: ");
-                posicionXp1[i - 1] = scanner.nextInt();
-                if (posicionXp1[i - 1] > tableroColumnas){
-                    System.out.println("Tu coordenada X no existe, intenta de nuevo:");
-                    posicionXp1[i - 1] = scanner.nextInt();
-                }
+            //Posiciones barcos jugador 1
+            ArrayList<Integer> posicionP1Y = new ArrayList<Integer>();
+            ArrayList<Integer> posicionP1X = new ArrayList<Integer>();
+            System.out.println("Jugador 1, Ingresa la posicion de tus barcos");
+            for (int i = 1; i <= nbarcos; i++) {
+                System.out.println("Barco " + i + " ingresa:");
+                System.out.print("Y:");
+                posicionP1Y.add(i - 1, scanner.nextInt());
+                System.out.print("X:");
+                posicionP1X.add(i - 1, scanner.nextInt());
             }
-            // Mostrando posiciones del jugador 1
-            for (int i = 1; i <= nbarcos; i++){
-                System.out.println("Posición barco " + i + " Y:" + posicionYp1[i - 1] + " X:" + posicionXp1[i - 1]);
+            System.out.print(posicionP1Y + " " + posicionP1X);
+
+
+            // Posiciones Jugador 2 (Maquina)
+            ArrayList<Integer> posicionP2Y = new ArrayList<>();
+            ArrayList<Integer> posicionP2X = new ArrayList<>();
+            System.out.println("\nJugador 2, Ingresa la posicion de tus barcos");
+            for (int i = 1; i <= nbarcos; i++) {
+                posicionP2Y.add(i - 1, (int)(Math.random()*tableroColumnas+1));
+                posicionP2X.add(i - 1, (int)(Math.random()*tableroFilas+1));
             }
+            Iterator<Integer> iteratorY = posicionP2Y.iterator();
+            Iterator<Integer> iteratorX = posicionP2X.iterator();
+            int j = 1;
+            while(iteratorY.hasNext() && iteratorX.hasNext()){
+                System.out.println("Juagor 2 ingresó:");
+                System.out.println("Barco " + j);
+                System.out.println("Y: " + iteratorY.next());
+                System.out.println("X: " + iteratorX.next());
 
-            //Aisgnando barcos jugador 2 (PC)
-            int[] posicionYp2 = new int[nbarcos];
-            int[] posicionXp2 = new int[nbarcos];
-
-            //Almacenando posiciones del jugador 2
-            System.out.println("Turno de que el jugador 2 asigne sus barcos");
-            for (int i = 1; i <= nbarcos; i++){
-                posicionYp2[i - 1] = (int)(Math.random()*tablefroFilas+1);
-                posicionXp2[i - 1] = (int)(Math.random()*tableroColumnas+1);
+                j++;
             }
+            System.out.print(posicionP2Y + " " + posicionP2X);
 
-            // Mostrando posiciones del jugador 2
-            for (int i = 1; i <= nbarcos; i++){
-                System.out.println("Posición barco " + i + " Y:" + posicionYp2[i - 1] + " X:" + posicionXp2[i - 1]);
-            }
-
-            //ADIVINANDO BARCOS
-            boolean estadoPartida = false;
-            int[] jugadorYp1 = new int[nCasillas];
-            int[] jugadorXp1 = new int[nCasillas];
-            int[] jugadorYp2 = new int[nCasillas];
-            int[] jugadorXp2 = new int[nCasillas];
-
-            int i = 0;
+            //ADIVIDAR Y JUGAR
+            //DATOS JUGADOR 1
+            ArrayList<Integer> tiradaP1Y = new ArrayList<>();
+            ArrayList<Integer> tiradaP1X = new ArrayList<>();
             int puntajeP1 = 0;
+            //DATOS JUGADOR 2
+            ArrayList<Integer> tiradaP2Y = new ArrayList<>();
+            ArrayList<Integer> tiradaP2X = new ArrayList<>();
             int puntajeP2 = 0;
 
-            System.out.println("Es momento de jugar!!!");
-            while (!estadoPartida) {
-                System.out.println("Turno del jugador 1\nIngresa las coordenadas a derribar");
-                System.out.print("Y:");
-                jugadorYp1[i] = scanner.nextInt();
-//                System.out.println(jugadorYp1[i]);
-                System.out.print("X:");
-                jugadorXp1[i] = scanner.nextInt();
-//                System.out.println(jugadorXp1[i]);
 
-                if (jugadorYp1[i] == posicionYp2[i] && jugadorXp1[i] == posicionXp2[i]){
-                    System.out.print("¡BOOOOMM! Barco derrivado");
-                    puntajeP1++;
-                    System.out.print("\nPuntaje actual: " + puntajeP1 + "/" + nbarcos + "\n");
+            System.out.println("\n!!HORA DE JUGAR");
+            boolean estadoJuego = false;
+            int i = 0;
+            while (!estadoJuego) {
+                if (!estadoJuego) {
+                    System.out.println("Turno jugador 1");
+                    System.out.println("Ingresa las coordenadas del barco a derribar:");
+                    System.out.print("Y:");
+                    tiradaP1Y.add(i, scanner.nextInt());
+                    System.out.print("X:");
+                    tiradaP1X.add(i, scanner.nextInt());
+                    if (posicionP2Y.contains(tiradaP1Y.get(i)) && posicionP2X.contains(tiradaP1X.get(i))){
+                        System.out.println("¡¡BOOOMMMM!!! Barco derribado por jugador 1");
+                        puntajeP1++;
+                        System.out.println("Puntaje acutual: " + puntajeP1 + "/" + nbarcos );
 
-                    if (puntajeP1 == nbarcos){
-                        System.out.println("¡¡FELICIDADES!! Jugador 1 ha ganado");
-                        System.out.println("Gracias por jugar");
-                        estadoPartida = true;
+                        if (puntajeP1 == nbarcos){
+                            System.out.println("¡¡¡FELICIDADESS!!!! Jugador 1 ha ganado");
+                            estadoJuego = true;
+                        }
                     }
                 }
 
-                System.out.println("Es turno del jugador 2\n Ingresa las coordenadas a derribar");
-                jugadorYp2[i] = (int)(Math.random()*tablefroFilas+1);
-                jugadorXp2[i] = (int)(Math.random()*tableroColumnas+1);
-                System.out.println("El segundo jugador tiró en Y:" + jugadorYp2[i] + " y en X:" + jugadorXp2[i]);
-                if (jugadorYp2[i] == posicionYp1[i] && jugadorXp2[i] == posicionXp1[i]){
-                    System.out.println("¡BOOOOMM! Barco derrivado");
-                    puntajeP2++;
-                    System.out.print("\nPuntaje actual: " + puntajeP2 + "/" + nbarcos + "\n");
+                if (!estadoJuego) {
+                    System.out.println("Turno jugador 2");
+                    tiradaP2Y.add(i,(int)(Math.random()*tableroColumnas+1));
+                    System.out.println("Jugador 2 tiró en Y: " + tiradaP2Y.get(i));
+                    tiradaP2X.add(i, (int)(Math.random()*tableroFilas+1));
+                    System.out.println("Jugador 2 tiró en X: " + tiradaP2X.get(i));
+                    if (posicionP1Y.contains(tiradaP2Y.get(i)) && posicionP1X.contains(tiradaP2X.get(i))){
+                        System.out.println("¡¡BOOOMMMM!!! Barco derribado por jugador 2");
+                        puntajeP2++;
+                        System.out.println("Puntaje acutual: " + puntajeP2 + "/" + nbarcos );
 
-                    if (puntajeP2 == nbarcos){
-                        System.out.println("¡¡FELICIDADES!! Jugador 2 ha ganado (te ganó una maquina xd)");
-                        System.out.println("Gracias por jugar");
-                        estadoPartida = true;
+                        if (puntajeP2 == nbarcos){
+                            System.out.println("¡¡¡FELICIDADESS!!!! Jugador 2 ha ganado");
+                            estadoJuego = true;
+                        }
                     }
                 }
-
-                if (i == nbarcos - 1 ){
-                    i = 0;
-                } else {
-                    i++;
-                }
+                i++;
             }
 
 
-//            int[] intentosY = new int[nCasillas - 1];
-//            int[] intentosX = new int[nCasillas - 1];
-//            int i = 0;
-//            while(!estadoPartida){
-//                System.out.print("Turno jugador 1.\n Ingres tu eje y:");
-//                intentosY[i] = scanner.nextInt();
-//                System.out.print("Ingres tu eje x:");
-//                intentosX[i] = scanner.nextInt();
-//
-//                System.out.println("Eje y:" + String.valueOf(intentosY[i]) + " Eje x:" +  String.valueOf(intentosX[i]));
-//                if (i == nbarcos) {
-//                    estadoPartida = true;
-//                }
-//                i++;
-//            }
-
-
-            System.out.println("Modo 1 solo jugador");
-//            int[] array = new int[10];
-//            for (int i = 0; i <= 9; i++){
-//                array[i] = i;
-//                System.out.println(array[i]);
-//            }
-
         } else {
-
-            System.out.println("Modo 2 juagadores");
-
+            System.out.print("Modo 2 juagadores");
         }
-
-
-
 
 
         //Cuadrado relleno
